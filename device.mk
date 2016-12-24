ec6cd1e529e4ede9424c324a9bf23197a76f7e57
fezzy102 <fezzy102@gmail.com>
2016-12-24 17:20:16 -0500

    Change back to stock DPI.

--- device.mk
+++ device.mk
@@ -297,7 +297,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=520
+    ro.sf.lcd_density=560
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true \
